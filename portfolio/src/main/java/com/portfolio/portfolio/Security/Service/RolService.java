19e4a9b3ef7a1008e69e0c4a5a9f91be89923d56
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-15 21:35:04 -0300

    upload

---
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Service/RolService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.portfolio.Security.Service;
+
+import com.portfolio.portfolio.Security.Entity.Rol;
+import com.portfolio.portfolio.Security.Enums.RolNombre;
+import com.portfolio.portfolio.Security.Repository.iRolRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author maxim
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
---
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Service/UsuarioService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.portfolio.Security.Service;
+
+import com.portfolio.portfolio.Security.Entity.Usuario;
+import com.portfolio.portfolio.Security.Repository.iUsuarioRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author maxim
+ */
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+    
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByEmail(String email){
+        return iusuarioRepository.existsByEmail(email);
+    }
+    
+    public void save(Usuario usuario){
+        iusuarioRepository.save(usuario);
+    }
+}
